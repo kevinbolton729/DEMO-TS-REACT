cdbaa17825bca4b29a31b7b1ab39d95a3bd125e5
kevinbolton729 <kbolton@sohu.com>
2018-04-19 21:51:47 +0800

    Revert "edit"
    This reverts commit ea95f1a017a9613fb71e4b9fa09c7f223a4b4707.

--- src/common/menu.js
+++ src/common/menu.js
@@ -13,6 +13,55 @@ const menuData = [
       },
     ],
   },
+  {
+    name: 'CMS管理',
+    icon: 'hdd',
+    path: 'cms',
+    children: [
+      {
+        name: '轮播图片',
+        path: 'sildes',
+      },
+      {
+        name: '设置栏目',
+        path: 'channels',
+      },
+      {
+        name: '所有内容',
+        path: 'articles',
+      },
+      {
+        name: '广告管理',
+        path: 'advers',
+      },
+    ],
+  },
+  {
+    name: '专题管理',
+    icon: 'bulb',
+    path: 'special',
+    children: [
+      {
+        name: '舒览品牌',
+        path: 'brand',
+      },
+      {
+        name: '实体店铺',
+        path: 'shop',
+      },
+    ],
+  },
+  {
+    name: '加盟申请',
+    icon: 'mail',
+    path: 'join',
+    children: [
+      {
+        name: '全部申请',
+        path: 'apply',
+      },
+    ],
+  },
 ];
 
 function formatter(data, parentPath = '', parentAuthority) {
