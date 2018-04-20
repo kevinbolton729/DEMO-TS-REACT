a2fc8b325b713bebbf9967685d4de36f1d0eddc9
kevinbolton729 <kbolton@sohu.com>
2018-04-20 14:15:06 +0800

    edit

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,20 +2,17 @@
  * @Author: Kevin Bolton
  * @Date: 2017-12-27 12:55:09
  * @Last Modified by: Kevin Bolton
- * @Last Modified time: 2018-04-20 10:59:17
+ * @Last Modified time: 2018-04-20 13:32:55
  */
-import React from 'react';
 import { Icon, Spin } from 'antd';
+import * as React from 'react';
+// 声明
+import { LoadingProps as Props } from './loading';
 
-const Loading = (props) => {
-  // 尺寸: 可选 small(默认) default large
-  // style: class 例如 {fontSize: 24}
-  // type: icon样式 例如 loading
-  // center: 无children，且需要居中是加上该属性
-  // children: 被Loading包裹的元素 例如 <Loading><div><span>loading...</span></div></Loading>
+export default (props: Props) => {
   const { size = 'small', style = {}, type = 'loading', center = false, children } = props;
   const newStyle = Object.assign({}, { fontSize: 24 }, style);
-  const newLoading = <Icon type="loading" style={newStyle} spin />;
+  const newLoading = <Icon type="loading" style={newStyle} spin={true} />;
 
   return type === 'default' ? (
     <Spin size={size} style={newStyle}>
@@ -25,5 +22,3 @@ const Loading = (props) => {
     <Spin indicator={newLoading}>{children || (center && <i />)}</Spin>
   );
 };
-
-export default Loading;
