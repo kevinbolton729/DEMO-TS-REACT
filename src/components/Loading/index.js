90d5d488c7ab94b41a9cae0f1537869bb1d01f33
kevinbolton729 <kbolton@sohu.com>
2018-04-20 10:59:31 +0800

    edit

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -2,13 +2,13 @@
  * @Author: Kevin Bolton
  * @Date: 2017-12-27 12:55:09
  * @Last Modified by: Kevin Bolton
- * @Last Modified time: 2018-04-20 10:51:21
+ * @Last Modified time: 2018-04-20 10:59:17
  */
 import React from 'react';
 import { Icon, Spin } from 'antd';
 
 const Loading = (props) => {
-  // 尺寸1: 可选 small(默认) default large
+  // 尺寸: 可选 small(默认) default large
   // style: class 例如 {fontSize: 24}
   // type: icon样式 例如 loading
   // center: 无children，且需要居中是加上该属性
